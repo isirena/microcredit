79ebdf4b098a2e491ef0ca8eead7ded8853d5311
isirena <Iurie.Sirena@gmail.com>
2018-09-28 02:05:16 +0300

    Modificare

--- microcredit/src/main/java/com/Library.java
+++ microcredit/src/main/java/com/Library.java
@@ -8,6 +8,7 @@ package com;
  * 1. Modificare din third1
  * 2. Modificare din primary
  * 2. Modificare din thrid1
+ * 3. Modificare din second1 - 2018-09-28 02:04 
  */
 public class Library {
     public boolean someLibraryMethod() {
